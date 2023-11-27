552a6cdabe805a7ebd039d1b581748f5b29fa2fc
Mh-Monzil <monzil246@gmail.com>
2023-11-27 11:01:51 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name:"bat", price:"30$" },
+    {name:"ball", price:"5$" }
+  ]
+  const friends = [
+    {name:"monzil", age:20},
+    {name:"fahim", age:25},
+    {name:"hasan", age:12},
+    {name:"ibrahim", age:30}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          friends.map(frnd => <Friend friend={frnd}></Friend>)
+        }
+        <Product name={products[0].name} price={products[0].price}></Product>
+        <Product name={products[1].name} price={products[1].price}></Product>
+        <p>Player Details</p>
+        <Player name="Mashrafi" ability="Bowler"></Player>
+        <Player name="Shakib" ability="All-rounder"></Player>
+        <Player name="Tamim" ability="Batter"></Player>
+        <Player name="Mushfiq" ability="Wicket-keeper"></Player>
+        <Player name="Riyad" ability="All-rounder"></Player>
       </header>
     </div>
   );
+}
+
+function Friend(props){
+  const {name, age} = props.friend;
+  console.log(name, age)
+  return (
+    <div>  
+      <h2>{name}</h2>
+      <h4>{age}</h4>
+    </div>
+    )
+}
+function  Product(props){
+
+  return (
+    <div style={{backgroundColor:"black",margin:"20px",padding:"50px"}}>
+      <h4 style={{}}>Product : {props.name}</h4>
+      <h5>Price : {props.price}</h5>
+    </div>
+  )
+}
+
+function Player(props){
+  const css = {
+    width: "70%",
+    border: "3px solid white",
+    margin: "20px"
+  }
+  const nameColor = {
+    color:"red"
+  }
+  const skillColor = {
+    color:"lime"
+  }
+  return (
+  <div style={css}>  
+    <h2 style={nameColor}>Name : {props.name}</h2>
+    <h4 style={skillColor}>Skill : {props.ability}</h4>
+  </div>
+  )
 }
 
 export default App;
